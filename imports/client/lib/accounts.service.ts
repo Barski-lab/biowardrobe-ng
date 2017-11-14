0656c8c86874b341f7dcc272dc02e8a95261b0d7
Michael Kotliar <misha.kotliar@gmail.com>
2017-11-14 10:38:44 -0500

    Closes #6, update to Observables
    - Replace auth.base with component.base to
      define more general class BWComponentBase
    - Add BWAccountService.login function.
      Refactored forgotPassword and resetPassword
      to return Observable.
    - Refactore BWForgot submit function. Tested,
      check comments for the return values.
    - Refactore BWLogin.submit function to use
      BWAccountService.login. Only `biowardrobeng`
      login method is supported.
    - Refactore BWReset.submit (check comments)
    - Add formGroup and related functions to
      BWServiceBase.
    - Add MeteorObservable

--- imports/client/lib/accounts.service.ts
+++ imports/client/lib/accounts.service.ts
@@ -1,23 +1,39 @@
 import { Meteor } from 'meteor/meteor';
 import { Injectable } from '@angular/core';
-import { BasicService } from './basic.service';
+import { Observable } from 'rxjs';
+import { BWServiceBase } from './service.base';
+
 
 @Injectable()
-export class AccountService extends BasicService {
+export class BWAccountService extends BWServiceBase {
 
     constructor() {
         super();
     }
 
-    forgotPassword(email:string):Promise<any> {
-        var accountFn = Accounts.forgotPassword.bind(null, {email: email});
-        return this.runWithPromise(accountFn);
+    forgotPassword(email:string):Observable<any> {
+        let accountFn = Observable.bindCallback(Accounts.forgotPassword);
+        return accountFn({email: email});
     }
 
-    resetPassword(token:string, newPassword:string):Promise<any> {
-        var accountFn = Accounts.resetPassword.bind(null, token, newPassword);
-        return this.runWithPromise(accountFn);
+    resetPassword(token:string, newPassword:string):Observable<any> {
+        let accountFn = Observable.bindCallback(Accounts.resetPassword);
+        return accountFn(token, newPassword);
     }
+
+    login(email:string, password:string):Observable<any> {
+        let loginObservable = Observable.bindCallback(function (callback) {
+            Accounts.callLoginMethod({
+                methodArguments: [{email: email, pass: password, biowardrobeng: true}],
+                userCallback: callback
+            });
+        });
+        return loginObservable();
+    }
+
 }
 
 
+export const ACCOUNTS_PROVIDERS: Array<any> = [
+    { provide: BWAccountService, useClass: BWAccountService }
+];
