8f118a59e080bf0dd05cc98c1e44a4a05e3f9340
Andrey Kartashov <porter@porter.st>
2019-06-27 11:21:14 -0400

    http proxy for SSL

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+
 import '../imports/server';
 import { setExtraUsers, configAccounts } from '../imports/server/modules/accounts'
+import { Log } from '../imports/server/modules/logger';
 
 const cors = require('cors');
+const fs = require('fs');
+const httpProxy = require('http-proxy');
 
 Meteor.startup(() => {
     // Throttle.setMethodsAllowed(false);            // Disable client-side methods
@@ -46,6 +50,35 @@ Meteor.startup(() => {
     if(Meteor.settings['cors_package']) {
         WebApp.rawConnectHandlers.use(cors());
     }
+
+    if(Meteor.settings['SSL']) {
+
+        const [,, host, targetPort] = Meteor.absoluteUrl().match(/([a-zA-Z]+):\/\/([\-\w\.]+)(?:\:(\d{0,5}))?/);
+
+        let proxy = httpProxy.createServer({
+            target: {
+                host,
+                port: process.env.PORT
+            },
+            ssl: {
+                key: fs.readFileSync(Meteor.settings['SSL'].key, 'utf8'),
+                cert: fs.readFileSync(Meteor.settings['SSL'].cert, 'utf8')
+            },
+            ws: true,
+            xfwd: true
+        }).listen(Meteor.settings['SSL'].port||targetPort);
+
+
+        proxy.on("error", function(err) {
+            Log.error("HTTP-PROXY NPM MODULE ERROR: ", err);
+            return;
+        });
+    }
+
+    // SSL(
+    //     Assets.getText("localhost.key"),
+    //     Assets.getText("localhost.cert"),
+    //     443);
 
     configAccounts();
     setExtraUsers();
