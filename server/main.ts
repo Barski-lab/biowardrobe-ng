c0620129afc6b0400158b1ad17529d92f3c5ed62
Andrey Kartashov <porter@porter.st>
2019-06-27 10:37:12 -0400

    update ostrio, add cors support settings.{cors, cors_package}

--- server/main.ts
+++ server/main.ts
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/server';
 import { setExtraUsers, configAccounts } from '../imports/server/modules/accounts'
 
-// const cors = require('cors');
+const cors = require('cors');
 
 Meteor.startup(() => {
     // Throttle.setMethodsAllowed(false);            // Disable client-side methods
@@ -33,30 +33,19 @@ Meteor.startup(() => {
     //     ].join(', '));
     //     return next();
     // });
-    // WebApp.rawConnectHandlers.use(function(req, res, next) {
-    //     res.setHeader("Access-Control-Allow-Origin", "*");
-    //     // add headers
-    //     res.setHeader('Access-Control-Allow-Headers', [
-    //         'Accept',
-    //         'Accept-Charset',
-    //         'Accept-Encoding',
-    //         'Accept-Language',
-    //         'Accept-Datetime',
-    //         'Authorization',
-    //         'Cache-Control',
-    //         'Connection',
-    //         'Cookie',
-    //         'Content-Length',
-    //         'Content-MD5',
-    //         'Content-Type',
-    //         'Date',
-    //         'User-Agent',
-    //         'X-Requested-With',
-    //         'Origin'
-    //     ].join(', '));
-    //     return next();
-    // });
-    // WebApp.rawConnectHandlers.use(cors());
+
+    if(Meteor.settings['cors']) {
+        WebApp.rawConnectHandlers.use(function (req, res, next) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader('Access-Control-Allow-Headers', "*");
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            res.setHeader('Access-Control-Expose-Headers', 'Content-Length, Content-Range, Content-Type');
+            return next();
+        });
+    }
+    if(Meteor.settings['cors_package']) {
+        WebApp.rawConnectHandlers.use(cors());
+    }
 
     configAccounts();
     setExtraUsers();
